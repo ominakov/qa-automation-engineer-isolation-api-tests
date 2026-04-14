254f73420b9446899017b7efe65809d947e8db9b
olegminakov <minakov82@gmail.com>
2026-04-14 22:23:40 +0300

    logger added

---
+++ tests/tools/logger.py
@@ -0,0 +1,28 @@
+import logging
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def get_test_logger(name: str) -> logging.Logger:
+    """
+    Возвращает настроенный логгер для тестового слоя.
+
+    Логгер кэшируется по имени, чтобы:
+    - не создавать дубликаты логгеров,
+    - избежать повторного добавления хендлеров,
+    - сохранить единый формат логирования.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+
+    return logger
